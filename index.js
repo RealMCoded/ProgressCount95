9857bc22b755fc2600f65c74e02a0ccb8620a2b3
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-05-16 09:37:46 -0400

    Added a toggle for Custom Emojis.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token, countingCh } = require('./config.json');
+const { token, countingCh, useCustomEmoji } = require('./config.json');
 
 const client = new Client({ ws: { properties: { $browser: "Discord iOS" }}, intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
@@ -17,6 +17,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('Ready!\n');
+	if (useCustomEmoji) {console.log("Custom Emoji support is on! Some emojis may fail to react if the bot is not in the server with the emoji.")} else {console.log("Custom Emoji support is off! No custom emojis will be used.")}
 	client.user.setActivity('counting', { type: 'COMPETING' });
 });
 
@@ -57,24 +58,28 @@ client.on('messageCreate', async message => {
 			if (lastCounterId !== message.author.id) {
 				var thec = message.content.split(' ')[0]
 				if (thec == String(numb+1)) {
-					switch (thec){
-						case "32": message.react("<:sys32:817120108761186405>"); break; //Sys32
-						case "67": message.react("<:pBarFlint67:892434159300120629>"); break; //pbar67
-						case "66": message.react("<:pBar66:868370162187927632>"); break; //pbar66
-						case "65": message.react("<:pBar65:856835166374199297>"); break; //pbar65
-						case "64": message.react("<:pBar64:853138163827212308>"); break; //pbar64
-						case "63": message.react("<:pBar63:840276684556337172>"); break; //pbar63
-						case "9000": message.react("<:ProgreshPower9000:825373078368288798>"); break; //power9000
-						case "800": message.react("<:ProgreshBC800:819147801425477652>"); break; //BC800
-						case "64": message.react("<:Progresh64KB:819147680089374730>"); break; //64KB
-						case "36": message.react("<:PBNOT36:819147051912134657>"); break; //NOT36
-						case "98": message.react("<:PB98:819146172928491580>"); break; //98
-						case "95": message.react("<:PB95:662601719653597196>"); break; //95
-						
-						//NOT BAR GAME RELATED BUT STILL FUNNY
-						case "100": message.react("💯"); break;
-						case "1984": message.react("<a:1984:971405081817804800>"); break;
-						default: message.react("✅"); break;
+					if (useCustomEmoji) {
+						switch (thec){
+							case "32": message.react("<:sys32:817120108761186405>"); break; //Sys32
+							case "67": message.react("<:pBarFlint67:892434159300120629>"); break; //pbar67
+							case "66": message.react("<:pBar66:868370162187927632>"); break; //pbar66
+							case "65": message.react("<:pBar65:856835166374199297>"); break; //pbar65
+							case "64": message.react("<:pBar64:853138163827212308>"); break; //pbar64
+							case "63": message.react("<:pBar63:840276684556337172>"); break; //pbar63
+							case "9000": message.react("<:ProgreshPower9000:825373078368288798>"); break; //power9000
+							case "800": message.react("<:ProgreshBC800:819147801425477652>"); break; //BC800
+							case "64": message.react("<:Progresh64KB:819147680089374730>"); break; //64KB
+							case "36": message.react("<:PBNOT36:819147051912134657>"); break; //NOT36
+							case "98": message.react("<:PB98:819146172928491580>"); break; //98
+							case "95": message.react("<:PB95:662601719653597196>"); break; //95
+							case "2": message.react("<:he3:975457560268275803>"); break; //2
+							//NOT BAR GAME RELATED BUT STILL FUNNY
+							case "100": message.react("💯"); break;
+							case "1984": message.react("<a:1984:971405081817804800>"); break;
+							default: message.react("✅"); break;
+						}
+					} else {
+						message.react("✅");
 					}
 					numb++
 					lastCounterId = message.author.id
@@ -105,5 +110,19 @@ client.on('messageCreate', async message => {
 	}
 
 });
+
+process.on('uncaughtException', (error, origin) => {
+    console.log('----- Uncaught exception -----')
+    console.log(error)
+    console.log('----- Exception origin -----')
+    console.log(origin)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('----- Unhandled Rejection at -----')
+    console.log(promise)
+    console.log('----- Reason -----')
+    console.log(reason)
+})
 
 client.login(token);
