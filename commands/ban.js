ed02e1c2e9c01018f5e5e7baecd31f47f20d8ac4
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-05-16 09:48:29 -0400

    Permission checks for ban and setcount

--- commands/ban.js
+++ commands/ban.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs')
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const {MessageEmbed} = require('discord.js');
+const { Permissions } = require('discord.js');
 const banMBRfile = './data/banned.json'
 const banMBR = require("." + banMBRfile);
 
@@ -11,6 +11,7 @@ module.exports = {
         .addUserOption(option => option.setName('user').setDescription('the user').setRequired(true))
         .addStringOption(option => option.setName('reason').setDescription('the reason for the ban. Unused for unbans.').setRequired(true)),
     async execute(interaction) {
+        if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) { //I'm using MANAGE_ROLES because it's a permission that is only available to all staff members - even helpers. This can be bumped to MANAGE_MEMBERS later.
         let mbr = interaction.options.getUser("user")
         if (mbr == "511223071504334866") {
             interaction.reply("You can't ban Chris from counting!")
@@ -30,8 +31,11 @@ module.exports = {
             });
 
             console.log(`${interaction.user.tag} banned ${mbr}`)
-        return interaction.reply({ content: `✅ **Banned ${mbr} from counting for "${interaction.options.getString("reason")}"**`, ephemeral: false }); 
+            return interaction.reply({ content: `✅ **Banned ${mbr} from counting for "${interaction.options.getString("reason")}"**`, ephemeral: false }); 
         }
         }
+    } else {
+        return interaction.reply({ content: `❌ **You cannot do this!**`, ephemeral: true });
+    }
     },
 };
