7e80713a2a6e6afbb03b31f78c2d3d6e7d336a58
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-05-29 14:28:54 -0400

    replaced chris antiban with bot & stuartt antiban

--- commands/ban.js
+++ commands/ban.js
@@ -11,9 +11,10 @@ module.exports = {
     async execute(interaction) {
         if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) { //I'm using MANAGE_ROLES because it's a permission that is only available to all staff members - even helpers. This can be bumped to MANAGE_MEMBERS later.
             let mbr = interaction.options.getUser("user")
-            if (mbr == "511223071504334866") {
-                interaction.reply("You can't ban Chris from counting!")
-                return
+            if (mbr == "513487616964952084") {
+                return interaction.reply("❌ **What did i ever do to you?**")
+            } else if (mbr == "511223071504334866") {
+                return interaction.reply("❌ **You cannot ban my creator from counting smh**")
             } else {
                 const db = interaction.client.db.Bans;
                 let ban = await db.findOne({ where: { userID: mbr.id } });
