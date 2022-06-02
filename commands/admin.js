9dadcddb87991e3133d5b17d25a020ef5ebd3de9
luihum <23081124+luihum@users.noreply.github.com>
2022-06-02 07:50:07 -0300

    merge admin commands - setban

--- commands/admin.js
+++ commands/admin.js
@@ -28,6 +28,30 @@ module.exports = {
                 .setName("reason")
                 .setDescription("the reason for the ban. Unused for unbans.")
                 .setRequired(true))),
-    async execute(interaction) {}
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand()
+        if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) { //I'm using MANAGE_ROLES because it's a permission that is only available to all staff members - even helpers. This can be bumped to MANAGE_MEMBERS later.
+            switch(subcommand) {
+                case "setban":
+                    let mbr = interaction.options.getUser("user")
+                    let ban = interaction.options.getBoolean("ban")
+                    let reason = interaction.options.getString("reason")
+                    if (mbr == "513487616964952084") {
+                        return interaction.reply("❌ **What did i ever do to you?**")
+                    } else {
+                        const db = interaction.client.db.Bans;
+                        if (ban) {
+                            await db.findOrCreate({ where: { userID: mbr.id } })
+                            interaction.reply(`✅ **Banned ${mbr.username}#${mbr.discriminator} from counting for "${interaction.options.getString("reason")}".**`)
+                        } else {
+                            await db.destroy({ where: { userID: mbr.id } })
+                            interaction.reply(`✅ **Unbanned ${mbr.username}#${mbr.discriminator} from counting.**`)
+
+                        }
+                    }
+            }
+        
+        }
+    }
 
 }
