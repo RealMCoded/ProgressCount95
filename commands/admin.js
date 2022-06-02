cb9b3995854f0e74a8f145abcbb1482eadca6726
luihum <23081124+luihum@users.noreply.github.com>
2022-06-02 11:40:06 -0300

    merging admin commands - banlist

--- commands/admin.js
+++ commands/admin.js
@@ -27,7 +27,10 @@ module.exports = {
             .addStringOption(option => option
                 .setName("reason")
                 .setDescription("the reason for the ban. Unused for unbans.")
-                .setRequired(true))),
+                .setRequired(true))
+        .addSubcommand(subcommand => subcommand
+            .setName("banlist")
+            .setDescription("Look at all the dead people."))),
     async execute(interaction) {
         const subcommand = interaction.options.getSubcommand()
         if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) { //I'm using MANAGE_ROLES because it's a permission that is only available to all staff members - even helpers. This can be bumped to MANAGE_MEMBERS later.
@@ -57,6 +60,33 @@ module.exports = {
 
                     console.log(`${interaction.user.tag} changed the number to ${numb}`)
                     return interaction.reply({ content: `✅ **Set the count to ${numb}!**`, ephemeral: false });
+                case "banlist":
+                    let banlist = '';
+                    const db = interaction.client.db.Bans;
+
+                    let bans = await db.findAll();
+                    if (bans.length == 0) {
+
+                    } else {
+                        for (let i = 0; i < bans.length; i++) {
+                            //TODO: Fix caching.
+                            let user = await interaction.client.users.cache.get(bans[i].userID);
+                            if (user) {
+                                banlist += `**${user.username}#${user.discriminator}** - ${bans[i].reason}\n`
+                            } else {
+                                banlist += `**<@${bans[i].userID}>**  - ${bans[i].reason}\n`
+                            }
+                        }
+                    }      
+                    if (banlist == '') {
+                        banlist = '**No one is banned from counting.**'
+                    }
+                    //create message embed
+                    const embed = new MessageEmbed()
+                        .setTitle('List of banned members from Counting - Work in progress')
+                        .setDescription(banlist)
+                        .setColor('#ff0000')
+                    return interaction.reply({embeds: [embed]}); 
             }
         
         } else {
