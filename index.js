a695f6d81da89cb29da6f620ce30aa45081ac810
luihum <23081124+luihum@users.noreply.github.com>
2022-06-06 20:24:06 -0300

    disable error handler

--- index.js
+++ index.js
@@ -54,19 +54,19 @@ client.on('interactionCreate', async interaction => {
 
 	if (!command) return;
 
-	try {
+	//try {
 		await command.execute(interaction);
 		
 		var numbdb = await client.db.Data.findOne({ where: { guildID: interaction.guild.id }})
 		numb = parseInt(numbdb.get("count"))
-	} catch (error) {
+	/*} catch (error) {
 		console.log(`${error}\n\n`)
 		if (interaction.user.id !== "284804878604435476") {
             await interaction.reply({content: `if you are seeing this, <@284804878604435476> messed up somehow. send this error to him plz :)\n\n\`\`\`${btoa(error)}\`\`\``, ephemeral: true})
         } else {
             await interaction.reply({content: `wow good job you fucked something up (again)\n\n\`\`\`${error}\`\`\``, ephemeral: true})
         }
-	}
+	}*/
 });
 
 client.on('messageCreate', async message => {
