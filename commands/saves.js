89357939e8be84d12bba0f3dc14bbdadfebea2cc
luihum <23081124+luihum@users.noreply.github.com>
2022-06-06 11:05:10 -0300

    fix saves command

--- commands/saves.js
+++ commands/saves.js
@@ -13,39 +13,31 @@ module.exports = {
 			.setDescription("View your saves!")
             .addUserOption(o => o
                 .setName("user")
-                .setDescription("The user you want to see how many save he/she have")))
-        .addSubcommand(subcommand => subcommand
-            .setName("server99")
-            .setDescription("Admin and debug use only! gives the server 99 saves")),
+                .setDescription("The user you want to see how many save he/she have"))),
     async execute(interaction) {
         const db = interaction.client.db.Saves;
         const subcommand = interaction.options.getSubcommand();
         let save = await db.findOne({ where: { userID: interaction.user.id } });
         
         if (subcommand === "claim") {
-
-            if (save) {
-                save.increment('saves', { by: 0.5 });
-            } else {
-                await db.create({
-                    userID: interaction.user.id,
-                    saves: 2.5
-                })
-            }
+            const [row,] = await db.findOrCreate({ where: { userID: interaction.user.id }, defaults: { saves: 2, slots: 5 } })
+            row.increment('saves', { by: 0.5 });
             const embed = new MessageEmbed()
                 .setTitle(`Saves`)
                 .setColor("#0099ff")
-                .setDescription(`You have claimed **0.5** saves!\nYou now have **${save.get('saves')+0.5}** saves!`)
+                .setDescription(`You have claimed **0.5** saves!\nYou now have **${row.get('saves')+0.5}** saves!`)
                 .setTimestamp()
             return interaction.reply({embeds: [embed]});
 
         } else if (subcommand === "view") {
+            
             if (interaction.options.getUser("user").id) save = interaction.options.getUser("user").id
-            if (save) {
+            const row = await db.findOne({ where: { userID: interaction.user.id }})
+            if (row) {
                 const embed = new MessageEmbed()
                     .setTitle(`Saves`)
                     .setColor("#0099ff")
-                    .setDescription(`You have **${save.get('saves')}** saves`)
+                    .setDescription(`You have **${row.get('saves')}** saves`)
                     .setTimestamp()
                 return interaction.reply({embeds: [embed]});
             } else {
@@ -55,16 +47,6 @@ module.exports = {
                     .setDescription(`**You have not claimed any saves! Use \`/saves claim\`!**`)
                     .setTimestamp()
                 return interaction.reply({embeds: [embed]});
-            }
-        } else if (subcommand === "server99") {
-            if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) { //I'm using MANAGE_ROLES because it's a permission that is only available to all staff members - even helpers. This can be bumped to MANAGE_MEMBERS later.
-                db.create({
-                    saves: 99,
-                    userID: "660752537401688085", //treating the server as a user for ease of use.
-                })
-                return interaction.reply({ content: "k", ephemeral: true }); //show nothing for now
-            } else {
-                return interaction.reply({ content: "You do not have permission to use this command", ephemeral: true });
             }
         }
     },
