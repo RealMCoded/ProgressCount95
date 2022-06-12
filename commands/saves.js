9862ba9e5fe2bdea9b0319d29faeecf97ea2f7dd
luihum <23081124+luihum@users.noreply.github.com>
2022-06-12 12:43:14 -0300

    testing save cooldown

--- commands/saves.js
+++ commands/saves.js
@@ -17,14 +17,11 @@ module.exports = {
         let save = await db.findOne({ where: { userID: interaction.user.id } });
         
         if (subcommand === "claim") {
-            const [row,] = await db.findOrCreate({ where: { userID: interaction.user.id }, defaults: { saves: 2, slots: 5 } })
-            console.log(row.saveCooldown)
+            let [row,] = await db.findOrCreate({ where: { userID: interaction.user.id }, defaults: { saves: 2, slots: 5 } })
+            let embed = new MessageEmbed().setTitle("Saves").setColor("#0099ff").setTimestamp()
             row.increment('saves', { by: 0.5 });
-            const embed = new MessageEmbed()
-                .setTitle(`Saves`)
-                .setColor("#0099ff")
-                .setDescription(`You have claimed **0.5** saves!\nYou now have **${row.get('saves')+0.5}** saves!`)
-                .setTimestamp()
+            if ((new Date().now - new Date(row.saveCooldown)) > 60*60*1000*12 ) { console.log("can claim!")}
+            `You have claimed **0.5** saves!\nYou now have **${row.get('saves')+0.5}** saves!`
             return interaction.reply({embeds: [embed]});
 
         } else if (subcommand === "view") {
