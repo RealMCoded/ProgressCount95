a7a44eb07f055785e3f5f99520600c167bf10951
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-06-15 09:22:53 -0400

    added a file to generate a messy "dataset" in json

---
+++ GenerateUserDataToJson.js
@@ -0,0 +1,41 @@
+const { generateDependencyReport } = require('@discordjs/voice');
+const { Sequelize } = require('sequelize');
+const { SQL_USER, SQL_PASS } = require('./config.json')
+const fs = require('node:fs')
+
+var data = new Array();
+
+//database shit
+const sequelize = new Sequelize('database', SQL_USER, SQL_PASS, {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	// SQLite only
+	storage: 'database.sqlite',
+});
+
+const db = require('./modal/database.js')
+
+gen()
+
+async function gen() {
+	var list = await db.Counters.findAll({
+		attributes: ['numbers', 'userID', 'wrongNumbers']
+	})
+
+
+	list = list.sort((a, b) => b.numbers - a.numbers)
+
+	for(var i=0; i < list.length; i++){
+		//console.log(`GOT ${list[i].userID} (${i+1} / ${list.length})`)
+		//push the userID and numbers to the data array
+		data.push({
+			userID: list[i].userID,
+			numbers: list[i].numbers,
+			wrongNumbers: list[i].wrongNumbers
+		})
+	}
+
+	fs.writeFileSync('./GeneratedUserData.json', JSON.stringify({ data }));
+	console.log('\nDone! Saved file as "GeneratedUserData.json"')
+}
