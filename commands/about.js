c076602fccff83564fe9a5149d2dc2c274dc9150
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-06-16 10:25:16 -0400

    /count now shows last counter

--- commands/about.js
+++ commands/about.js
@@ -7,11 +7,15 @@ module.exports = {
         .setName('about')
         .setDescription(`About the bot`),
     async execute(interaction) {
+        //todo: check if git is installed
+        revision = require('child_process')
+            .execSync('git rev-parse HEAD')
+            .toString().substring(0, 7);
         const currentTimestamp = Math.round(+new Date() / 1000);
         const timestampColour = currentTimestamp.toString(16).substring(2, 10);
         const embed = new MessageEmbed()
             .setTitle('ProgressCount95')
-            .setDescription(`Version ${project.version}\n\nBot created by stuartt#2419, Assisted by Luihum#1287 and 5jiji#2022`)
+            .setDescription(`Version ${project.version} (commit \`${revision}\`)\n\nBot created by stuartt#2419, Assisted by Luihum#1287 and 5jiji#2022`)
             .setColor(`${timestampColour}`);
         await interaction.reply({embeds: [embed]});
     },
