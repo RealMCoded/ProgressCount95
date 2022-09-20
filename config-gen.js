ec0a8650599e5c145c6998cf4df2d1e7b354113a
5jiji <60753482+5jiji@users.noreply.github.com>
2022-09-20 21:09:11 +0200

    New config generator

--- config-gen.js
+++ config-gen.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -9,56 +9,47 @@ const rl = readline.createInterface({
 console.log("\nProgressCount95 First run config.json creator\n")
 
 if (fs.existsSync('config.json')) {
-    console.log("!!! config.json already exists. Please delete it and try again. !!!\n")
-    process.exit(1)
+  console.log("!!! config.json already exists. Please delete it and try again. !!!\n")
+  process.exit(1)
 }
 
-rl.question('Enter your bot\'s ClientID: ', function (clientId) {
-    rl.question('Enter the ID of the server you will use the bot in: ', function (guildId) {
-        rl.question('Enter the ID of the Counting Channel: ', function (countingCh) {
-            rl.question('Enter your bot token: ', function (token) {
-                rl.question('Enter your SQL Username: ', function (SQL_USER) {
-                    rl.question('Enter your SQL Password: ', function (SQL_PASS) {
-                        rl.question('How many guild save slots? (default: 2): ', function (guildSaveSlots) {
-                            rl.question('How many user saves per guild saves? (default: 4): ', function (userSaveSlots) {
-                                rl.question('How many default save slots? (default: 5): ', function (defaultSlots) {
-                                    rl.question('How many saves should the user start with? (default: 2): ', function (initialSaves) {
-                                        rl.question('How many saves should the user claim with /saves claim (default: 0.5): ', function (savesPerClaim) {
-                                            rl.question('How many numbers are required for a free save (default: 50): ', function (numbersRequiredForFreeSave) {
-                                                rl.question('How many free saves should be given after that number? (default: 0.1): ', function (freeSave) {
-                                                    fs.writeFileSync('config.json', JSON.stringify({
-                                                        "token": token,
-                                                        "clientId": clientId,
-                                                        "guildId": guildId,
-                                                        "countingCh": countingCh,
-                                                        "useCustomEmoji": false, //force this to false
-                                                        "SQL_USER": SQL_USER,
-                                                        "SQL_PASS": SQL_PASS,
-                                                        "guildSaveSlots": parseInt(guildSaveSlots),
-                                                        "userSavesPerGuildSave": parseInt(userSaveSlots),
-                                                        "saveClaimCooldown": 43200,
-                                                        "defaultSlots": parseInt(defaultSlots),
-                                                        "initialSaves": parseInt(initialSaves),
-                                                        "savesPerClaim": parseFloat(savesPerClaim),
-                                                        "numbersRequiredForFreeSave": parseInt(numbersRequiredForFreeSave),
-                                                        "freeSave": parseInt(freeSave)
-                                                    }));
-                                                    rl.close();
-                                                });
-                                            });
-                                        });
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
-    });
+async function questions() {
+  let clientId = await new Promise(r => rl.question('Enter your bot\'s ClientID: ', r))
+  let guildId = await new Promise(r => rl.question('Enter the ID of the server you will use the bot in: ', r))
+  let countingCh = await new Promise(r => rl.question('Enter the ID of the Counting Channel: ', r))
+  let token = await new Promise(r => rl.question('Enter your bot token: ', r))
+  let SQL_USER = await new Promise(r => rl.question('Enter your SQL Username: ', r))
+  let SQL_PASS = await new Promise(r => rl.question('Enter your SQL Password: ', r))
+  let guildSaveSlots = await new Promise(r => rl.question('How many guild save slots? (default: 2): ', r))
+  let userSaveSlots = await new Promise(r => rl.question('How many user saves per guild saves? (default: 4): ', r))
+  let defaultSlots = await new Promise(r => rl.question('How many default save slots? (default: 5): ', r))
+  let initialSaves = await new Promise(r => rl.question('How many saves should the user start with? (default: 2): ', r))
+  let savesPerClaim = await new Promise(r => rl.question('How many saves should the user claim with /saves claim (default: 0.5): ', r))
+  let numbersRequiredForFreeSave = await new Promise(r => rl.question('How many numbers are required for a free save (default: 50): ', r));
+  let freeSave = await new Promise(r => rl.question('How many free saves should be given after that number? (default: 0.1): ', r))
+  
+  await fs.writeFile('config.json', JSON.stringify({
+    "token": token,
+    "clientId": clientId,
+    "guildId": guildId,
+    "countingCh": countingCh,
+    "useCustomEmoji": false, //force this to false
+    "SQL_USER": SQL_USER,
+    "SQL_PASS": SQL_PASS,
+    "guildSaveSlots": parseInt(guildSaveSlots),
+    "userSavesPerGuildSave": parseInt(userSaveSlots),
+    "saveClaimCooldown": 43200,
+    "defaultSlots": parseInt(defaultSlots),
+    "initialSaves": parseInt(initialSaves),
+    "savesPerClaim": parseFloat(savesPerClaim),
+    "numbersRequiredForFreeSave": parseInt(numbersRequiredForFreeSave),
+    "freeSave": parseInt(freeSave)
+  }));
+}
+
+rl.on('close', () => {
+  console.log("\nConfig.json created!")
+  process.exit(0);
 });
 
-rl.on('close', function () {
-    console.log("\nConfig.json created!")
-    process.exit(0);
-});
+questions().then(e => rl.close());
