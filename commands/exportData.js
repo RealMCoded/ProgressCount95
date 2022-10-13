c36fb6e3215d0543ceaafc6396f7b61d20c7ac5a
Luihum <luihumm@gmail.com>
2022-10-13 17:57:06 -0300

    fixed /export-leaderboard

--- commands/exportData.js
+++ commands/exportData.js
@@ -1,13 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const fs = require('node:fs')
+const { MessageAttachment } = require('discord.js');
+const fs = require('node:fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('export-leaderboard')
 		.setDescription(`Exports leaderboard as a JSON file`),
 	async execute(interaction) {
-		interaction.reply("<a:typing:1017890251236200569> **The file is being generated and will be DMd to you soon!**\n\n**Do not run this command as this process is happening, as it will break the bot.**")
-		
+		if(interaction.client.isDumping) {
+			return interaction.reply("<:CountingWarn:981961793515716630> **The bot is currently generating a leaderboard file, please wait until this is finished before requesting.**")
+		}
+		interaction.client.isDumping = true
+		await interaction.reply("<a:typing:1017890251236200569> **The file is being generated and will be sent to you soon!**")
+		console.log(`${interaction.user.tag} requested leaderboard dump`)
 		let db = interaction.client.db
 		var data = new Array();
 
@@ -33,7 +38,9 @@ module.exports = {
 				numbers: list[i].numbers,
 			})
 		}
+		interaction.client.isDumping = false
+		const dump = new MessageAttachment(Buffer.from(JSON.stringify(data)), "leaderboard.json")
+		interaction.followUp({ content: `${interaction.user} Here's your leaderboard export file!`, files: [dump], ephemeral: true })
 
-		interaction.user.send(`\`\`\`json\n${JSON.stringify(data)}\n\`\`\``)
 	},
 };
