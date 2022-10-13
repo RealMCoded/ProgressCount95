edcfc6398b34286c3ab7cb2bd9077c1801e42a21
Luihum <luihumm@gmail.com>
2022-10-13 08:32:18 -0300

    Fix ignored emoji when really long message

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ client.on('messageCreate', async message => {
 							}, 3000);
 						if (message.content.length >= 1500){
 							//sure we can just ignore it but it's funnier when the bot replies lol
-							if (useCustomEmoji) {message.react(customEmojiList.i)} else {message.react("⛔")}
+							if (useCustomEmoji) {message.react(customEmojiList.ignored)} else {message.react("⛔")}
 							message.reply("https://cdn.discordapp.com/attachments/875920385315577867/927848021968949268/Screenshot_20220103-225144.jpg?size=4096")
 						} else if (lecountr.saves >= 10) {
 							if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
