14166d9ade8783e417df7459740d107479cb53cb
Luihum <luihumm@gmail.com>
2022-10-20 11:40:24 -0300

    fix last counter id ruin restart bug

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ client.on('messageCreate', async message => {
 							message.reply(`${message.author} ruined the count!\nThe next number was **${numb+1}**, but they said **${thec}**!\nThe next number is **1** | **Wrong Number.**`)
 							numb = 0
 							lastCounterId = "0"
-							guildDB.update({ lastCounterId: "0" })
+							guildDB.update({ lastCounterID: "0" })
 							if (logRuins) console.log(`${message.author.tag} ruined the count at ${numb}!`)
 						}
 						lecountr.increment('wrongNumbers');
@@ -270,7 +270,7 @@ client.on('messageCreate', async message => {
 						message.reply(`${message.author} ruined the count!\nThe next number was **${numb+1}**, but they said **${thec}**! | **You cannot count more than one time in a row**!`)
 						numb = 0
 						lastCounterId = "0"
-						guildDB.update({ lastCounterId: "0" })
+						guildDB.update({ lastCounterID: "0" })
 						if (logRuins) console.log(`${message.author.tag} ruined the count at ${numb}!`)
 					}
 					lecountr.increment('wrongNumbers');
