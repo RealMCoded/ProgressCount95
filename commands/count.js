3282d9dbae93e219608a58426f1db72702107957
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-10-20 21:58:46 -0400

    Update count.js

--- commands/count.js
+++ commands/count.js
@@ -1,11 +1,9 @@
-const fs = require('node:fs')
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('count')
         .setDescription(`Get the current count`),
-        //.addIntegerOption(option => option.setName('numb').setDescription('the number').setRequired(true)),
     async execute(interaction) {
 
         const guildDB = await interaction.client.db.Data.findOne({ where: { guildID: interaction.guild.id }});
