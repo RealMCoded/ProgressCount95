6c07082c3d1b0ea7316067dbfe3b56837d5c3e50
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-11-30 16:12:24 -0500

    more util...

--- RUN.bat
+++ RUN.bat
@@ -1,1 +1,1 @@
-node .
+npm run start
--- Util.js
+++ Util.js
@@ -1,5 +1,13 @@
 module.exports = {
 	validateExpression(number) {
 		return /^[+\-/*^0-9().]+$/.test(number)
-	}
+	},
+
+	random_range(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    },
+
+    random(number) {
+        return Math.floor(Math.random() * number)
+    }
 }
