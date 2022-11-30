9307553e226bd84e726700e5c69798d3c0cbbd6c
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-11-30 16:34:39 -0500

    Calculate context menu command

---
+++ commands/contextmenu_message_calculate.js
@@ -0,0 +1,20 @@
+const { ContextMenuCommandBuilder } = require('@discordjs/builders');
+const { ApplicationCommandType } = require('discord-api-types/v9');
+const mathx = require('math-expression-evaluator');
+const { validateExpression }= require("../Util.js");
+
+module.exports = { 
+    data: new ContextMenuCommandBuilder()
+        .setName('Calculate')
+        .setType(ApplicationCommandType.Message),
+    async execute(interaction) {
+        const input = interaction.targetMessage.content
+
+        if(validateExpression(input)) {
+            const output = mathx.eval(input)
+            interaction.reply({ content: `The result is **${output}**`, ephemeral: true})
+        } else {
+            interaction.reply({ content: `Invalid expression. Make sure there are no invalid operators or spaces and try again.`, ephemeral: true })
+        }
+    }
+}
