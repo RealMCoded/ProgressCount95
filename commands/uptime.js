e05e55f5c226987d8ce0bbaf0c654452dd6ba45c
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-12-29 15:03:21 -0500

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('uptime')
-		.setDescription(`get how long the bot has been online`),
+		.setDescription(`Get how long the bot has been online`),
 	async execute(interaction) {
 		const uptime = `${Math.floor((Math.round(process.uptime() * 100) / 100)/60)} minutes`;
 
