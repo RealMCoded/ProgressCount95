2f9f92a266dd23e6b500780d70095318e0b6a534
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-04-22 12:53:41 -0400

    slight index rewrite to be more cleaner

--- Util.js
+++ Util.js
@@ -2,16 +2,20 @@
     This file contains some functions that are re-sued by other files in the bot.
 */
 
-module.exports = {
-	validateExpression(number) {
-		return /^[+\-/*^0-9().]+$/.test(number)
-	},
-
-	random_range(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    },
-
-    random(number) {
-        return Math.floor(Math.random() * number)
-    }
+function validateExpression(number) {
+    return /^[+\-/*^0-9().]+$/.test(number)
 }
+
+function random_range(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function random(number) {
+    return Math.floor(Math.random() * number)
+}
+
+function isNumber(message) {
+    return validateExpression(message.content.split(" ")[0]) && message.attachments.size == 0 && message.stickers.size == 0 && message.content.toUpperCase() !== "INFINITY"
+}
+
+module.exports = { validateExpression, random_range, random, isNumber }
--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const fs = require('node:fs');
-const Sequelize = require('sequelize');
 const { Client, Collection, Intents, WebhookClient } = require('discord.js');
 const { token, countingCh, useCustomEmoji, numbersRequiredForFreeSave, freeSave, saveClaimCooldown, logHook, redirectConsoleOutputToWebhook, customEmojiList, longMessageEasterEggContent, longMessageEasterEgg, ruinDelay, nerdstatExecutor, guildId, logRuins, logSaveUses, enableRulesFile, showRulesOnFirstCount, status, fallbackToChannelIfDMFails } = require('./config.json');
 const mathx = require('math-expression-evaluator');
 const client = new Client({ ws: { properties: { browser: "Discord iOS" }}, intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
-const { validateExpression } = require('./Util.js')
+const { isNumber } = require('./Util.js')
 
-//database shit
-const sequelize = new Sequelize('database', "", "", {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	// SQLite only
-	storage: 'database.sqlite',
-});
 client.db = require('./database.js')
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 //redef some console functions here
-console.log = function(e) {
+console.log = async function(e) {
 	try {
 		if (redirectConsoleOutputToWebhook) {
 			let webhookClient = new WebhookClient({ url: logHook });
@@ -32,7 +23,7 @@ console.log = function(e) {
 	process.stdout.write(`${e}\n`);
 }
 
-console.warn = function(e) {
+console.warn = async function(e) {
 	try {
 		if (redirectConsoleOutputToWebhook) {
 			let webhookClient = new WebhookClient({ url: logHook });
@@ -44,7 +35,7 @@ console.warn = function(e) {
 	process.stdout.write(`[WARN] ${e}\n`);
 }
 
-console.error = function(e) {
+console.error = async function(e) {
 	try {
 		if (redirectConsoleOutputToWebhook) {
 			let webhookClient = new WebhookClient({ url: logHook });
@@ -56,10 +47,9 @@ console.error = function(e) {
 	process.stdout.write(`[ERROR] ${e}`);
 }
 
-const recentCountRuiners= new Set();
-
 var canAllCount = true
 var lastCounterId, serverSaves, guildDB, highscore, streak
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
@@ -85,16 +75,16 @@ client.once('ready', async () => {
 	if (status.enableActivity) {
 		client.user.setActivity(`${status.activity_name} ${(status.showCurrentNumber ? `| ${numb}` : '')}`, { type: status.activity_type });
 
-		setInterval(() => {
-			client.user.setActivity(`${status.activity_name} ${(status.showCurrentNumber ? `| ${numb}` : '')}`, { type: status.activity_type });
-		}, 90000);
+		if (status.showCurrentNumber) {
+			setInterval(() => {
+				client.user.setActivity(`${status.activity_name} ${(status.showCurrentNumber ? `| ${numb}` : '')}`, { type: status.activity_type });
+			}, 90000);
+		}
 	}
 });
 
 //All slash commands. check "commands" folder
 client.on('interactionCreate', async interaction => {
-	//if (!interaction.isCommand()) return;
-
 	const command = client.commands.get(interaction.commandName);
 
 	if (!command) return;
@@ -129,166 +119,101 @@ client.on('messageCreate', async message => {
 		default: return;
 	}
 
-	if (message.channel.id === countingCh) {
+	//ONLY execute if it's in the desired counting channel
+	if (message.channel.id === countingCh && isNumber(message)) {
+
+		//Age Gate
 		if (Date.now() - message.author.createdAt < 0) {
-			if (validateExpression(message.content.split(" ")[0]) && message.attachments.size == 0 && message.stickers.size == 0 && message.content.toUpperCase() !== "INFINITY") { 
-				if (useCustomEmoji) {message.react(customEmojiList.ignored)} else {message.react("⛔")}
-				message.reply("⚠️ **Your account is too young to count! Your account must be 7 days old to count.**")
-				return;
-			}
-		  }
-
-		let [lecountr, isNewCounter] = await client.db.Counters.findOrCreate({ where: { userID: message.author.id } });
-
-		if (isNewCounter && enableRulesFile && showRulesOnFirstCount) {
-			message.reply(fs.readFileSync('./rules.txt',{encoding:'utf8', flag:'r'}));
+			if (useCustomEmoji) {message.react(customEmojiList.ignored)} else {message.react("⛔")}
+			return message.reply("⚠️ **Your account is too young to count! Your account must be 7 days old to count.**")
 		}
 
-		if (lecountr.banned) {
-			if (validateExpression(message.content.split(" ")[0]) && message.attachments.size == 0 && message.stickers.size == 0 && message.content.toUpperCase() !== "INFINITY") { 
-				if (useCustomEmoji) {message.react(customEmojiList.ignored)} else {message.react("⛔")}
-			}
-			return;
-		}
+		//define the user that sent the number
+		let [user_counter, isNewCounter] = await client.db.Counters.findOrCreate({ where: { userID: message.author.id } });
 
-		//check if first string in message is a math expression
-		if (validateExpression(message.content.split(" ")[0]) && message.attachments.size == 0 && message.stickers.size == 0 && message.content.toUpperCase() !== "INFINITY") { //MAKE INFINITY DETECTION BETTER
-			try {
-				var thec = mathx.eval(message.content.split(' ')[0])
-			} catch(e) {return}
-			if (!isNaN(thec)) {
-				if (lastCounterId !== message.author.id) {
-					if (thec == String(numb+1)) {
-						if (useCustomEmoji) {
-							if (thec in customEmojiList.other) {
-								message.react(customEmojiList.other[thec])
-							} else {
-								let hasReacted=0
-								for (const [key,value] of Object.entries(customEmojiList.milestones).reverse()) {
-									let remainder = thec % +key
-									if (remainder === 0) {
-										message.react(value)
-										hasReacted = 1 
-										break;
-									}
-								}
-								for (const [key,value] of Object.entries(customEmojiList.endings)) {
-									if (thec.toString().endsWith(key)) {
-										message.react(value)
-										hasReacted = 1 
-										break;
-									}
-								}
-								if (!hasReacted) {
-									if (thec.toString().length > 2 && thec.toString() == thec.toString().split('').reverse().join('')) {
-										message.react(customEmojiList.palindrome)
-									} else if (highscore < thec) {
-										message.react(customEmojiList.highscore)
-									} else {
-										message.react(customEmojiList.normal)
-									}
-								}
-							}
- 						} else {
-							switch (thec){
-								case 100: message.react("💯"); break;
-								case 420: message.react("🌿"); break;
-								default: if (highscore < thec) message.react("☑"); else message.react("✅"); break;
-							}
-						}
-						numb++
-						streak++
-						if (highscore < numb) highscore = numb;
-						lastCounterId = message.author.id
-						guildDB.update({ lastCounterID: message.author.id })
-						lecountr.increment('numbers');
-						
-						if(lecountr.numbers % numbersRequiredForFreeSave == 0) { //every 50(by default) numbers
-							//lecountr.increment('saves', { by: freeSave.toFixed(1) })
-							if (lecountr.saves >= lecountr.slots*10) return;
-							let freeSaveClamped
-							if ((lecountr.saves + freeSave) > lecountr.slots*10) {
-								freeSaveClamped = lecountr.slots*10 - lecountr.saves 
-								console.log(`clamped: ${freeSaveClamped}`)
-							} else freeSaveClamped = freeSave
-							lecountr.update({saves: (lecountr.saves + freeSaveClamped)})
-							lecountr = await client.db.Counters.findOne({ where: { userID: message.author.id } });
-						}
-					} else {
-						canAllCount = false;
-							setTimeout(() => {
-								canAllCount = true;
-							}, ruinDelay);
-						if (message.content.length >= 1500){
-							if (useCustomEmoji) {message.react(customEmojiList.ignored)} else {message.react("⛔")}
-							if (longMessageEasterEgg) {
-								message.reply(longMessageEasterEggContent)
-							}
-						} else if (numb == 0){
-							if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
-							message.reply(`${message.author} tried to ruin the count, but the number was already **0**!\nNo stats (saves, incorrect numbers) have been modified.`)
-							//this is the way to bypass the increment of wrong numbers. It removes and then adds one, cancelling it out.
-							lecountr.decrement('wrongNumbers');
-						} else if (lecountr.saves >= 10) {
-							if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
-							const ten = 10
-							lecountr.decrement('saves', { by: ten})
-							message.reply(`${message.author} almost ruined the count, but one of their saves were used!\n${message.author.tag} now has **${(lecountr.saves-10)/10}** saves remaining.\nThe next number is **${numb + 1}** | **Wrong Number.**\n**STREAK LOST AT ${streak}!**`)
-							if (logSaveUses) console.log(`${message.author.tag} used one of their saves, now they have ${(lecountr.saves-1)/10}`)
-						} else if (serverSaves >= 1) {
-							if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
-							serverSaves--
-							message.reply(`${message.author} almost ruined the count, but a server save was used!\n**${serverSaves}** server saves remain.\nThe next number is **${numb+1}** | **Wrong Number.**\n**STREAK LOST AT ${streak}!**`)
-							if (logSaveUses) console.log(`${message.author.tag} used a server save, now the server has ${serverSaves}}!`)
-						} else {
-							if (useCustomEmoji) {message.react(customEmojiList.ruin)} else {message.react('❌')}
-							message.reply(`${message.author} ruined the count!\nThe next number was **${numb+1}**, but they said **${thec}**!\nThe next number is **1** | **Wrong Number.**\n**STREAK LOST AT ${streak}!**`)
-							numb = 0
-							lastCounterId = "0"
-							guildDB.update({ lastCounterID: "0" })
-							if (logRuins) console.log(`${message.author.tag} ruined the count at ${numb}!`)
-						}
-						lecountr.increment('wrongNumbers');
-						streak = 0
-					}
+		//send rules if they are a new counter AND if rules are enabled
+		if (isNewCounter && enableRulesFile && showRulesOnFirstCount) {message.reply(fs.readFileSync('./rules.txt',{encoding:'utf8', flag:'r'}));}
+
+		//if they are banned react with the ignored emoji and end execution.
+		if (user_counter.banned) {if (useCustomEmoji) {message.react(customEmojiList.ignored)} else {message.react("⛔")}}
+
+		//actual counting logic
+		try {
+			var user_count = mathx.eval(message.content.split(' ')[0])
+		} catch(e) {return} //just incase it fails
+
+		if (!isNaN(user_count)) {
+			//fails
+			if (lastCounterId == message.author.id) {return fail("2_IN_A_ROW", user_counter, message, user_count);} //2 times in a row
+			if (user_count != String(numb+1)) {return fail("NUMBER_WRONG", user_counter, message, user_count);} //wrong number
+
+			//add message reaction
+			if (useCustomEmoji) {
+				if (user_count in customEmojiList.other) {
+					message.react(customEmojiList.other[user_count])
 				} else {
-					canAllCount = false;
-						setTimeout(() => {
-							canAllCount = true;
-						}, 3000);
-					if (lecountr.saves >= 10) {
-						if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
-						lecountr.decrement('saves', {by: 10})
-						message.reply(`${message.author} almost ruined the count, but one of their saves were used!\n${message.author.tag} now has **${(lecountr.saves-10)/10}** saves remaining.\nThe next number is **${numb + 1}** | **You cannot count twice in a row!**\n**STREAK LOST AT ${streak}!**`)
-						if (logSaveUses) console.log(`${message.author.tag} used one of their saves, now they have ${(lecountr.saves-10)/10}`)
-					} else if (numb == 0){
-						if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
-						message.reply(`${message.author} tried to ruin the count, but the number was already **0**!\nNo stats (saves, incorrect numbers) have been modified.`)
-						//this is the way to bypass the increment of wrong numbers. It removes and then adds one, cancelling it out.
-						lecountr.decrement('wrongNumbers');
-					} else if (serverSaves >= 1) {
-						if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
-						serverSaves--
-						message.reply(`${message.author} almost ruined the count, but a server save was used!\n**${serverSaves}** server saves remain.\nThe next number is **${numb+1}** | **You cannot count more than one time in a row**!\n**STREAK LOST AT ${streak}!**`)
-						if (logSaveUses) console.log(`${message.author.tag} used a server save, now the server has ${serverSaves}}!`)
-					} else {
-						if (useCustomEmoji) {message.react(customEmojiList.ruin)} else {message.react('❌')}
-						message.reply(`${message.author} ruined the count!\nThe next number was **${numb+1}**, but they said **${thec}**! | **You cannot count more than one time in a row**!\n**STREAK LOST AT ${streak}!**`)
-						numb = 0
-						lastCounterId = "0"
-						guildDB.update({ lastCounterID: "0" })
-						if (logRuins) console.log(`${message.author.tag} ruined the count at ${numb}!`)
+					let hasReacted=0
+					for (const [key,value] of Object.entries(customEmojiList.milestones).reverse()) {
+						let remainder = user_count % +key
+						if (remainder === 0) {
+							message.react(value)
+							hasReacted = 1 
+							break;
+						}
 					}
-					lecountr.increment('wrongNumbers');
-					streak = 0
+					for (const [key,value] of Object.entries(customEmojiList.endings)) {
+						if (user_count.toString().endsWith(key)) {
+							message.react(value)
+							hasReacted = 1 
+							break;
+						}
+					}
+					if (!hasReacted) {
+						if (user_count.toString().length > 2 && user_count.toString() == user_count.toString().split('').reverse().join('')) {
+							message.react(customEmojiList.palindrome)
+						} else if (highscore < user_count) {
+							message.react(customEmojiList.highscore)
+						} else {
+							message.react(customEmojiList.normal)
+						}
+					}
+				}
+			 } else {
+				switch (user_count){
+					case 100: message.react("💯"); break;
+					case 420: message.react("🌿"); break;
+					default: if (highscore < user_count) message.react("☑"); else message.react("✅"); break;
+				}
 			}
+
+			//increase local variables
+			numb++
+			streak++
+			if (highscore < numb) highscore = numb;
+			lastCounterId = message.author.id
+			guildDB.update({ lastCounterID: message.author.id })
+			user_counter.increment('numbers');
+
+			//free save
+			if(user_counter.numbers % numbersRequiredForFreeSave == 0) { //every 50(by default) numbers
+				//lecountr.increment('saves', { by: freeSave.toFixed(1) })
+				if (user_counter.saves >= user_counter.slots*10) return;
+				let freeSaveClamped
+				if ((user_counter.saves + freeSave) > user_counter.slots*10) {
+					freeSaveClamped = user_counter.slots*10 - user_counter.saves 
+					console.log(`clamped: ${freeSaveClamped}`)
+				} else freeSaveClamped = freeSave
+				user_counter.update({saves: (user_counter.saves + freeSaveClamped)})
+				user_counter = await client.db.Counters.findOne({ where: { userID: message.author.id } });
+			}
+
 			guildDB.update({ count: numb, guildSaves: serverSaves, highscore: highscore, streak: streak })
 		}
-	}
 	}
 
 });
 
+//delete detection
 client.on('messageDelete', async message => {
 	if (message.author.bot) return
 
@@ -299,19 +224,72 @@ client.on('messageDelete', async message => {
 		default: return;
 	}
 
-	if (message.channel.id === countingCh) {
-		let [lecountr, ] = await client.db.Counters.findOrCreate({ where: { userID: message.author.id } });
+	if (message.channel.id === countingCh && isNumber(message)) {
+		let [user_counter, ] = await client.db.Counters.findOrCreate({ where: { userID: message.author.id } });
 
-		if (lecountr.banned) return;
+		if (user_counter.banned) return;
 
-		if (validateExpression(message.content.split(" ")[0]) && message.attachments.size == 0 && message.stickers.size == 0 && message.content.toUpperCase() !== "INFINITY") {
-			let thec = parseInt(message.content.split(' ')[0])
-			if (thec == String(numb)){
-				message.channel.send(`${message.author} deleted their number! The current number is **${numb}**.`)
-			}
+		let user_count = parseInt(message.content.split(' ')[0])
+		if (user_count == String(numb)){
+			message.channel.send(`${message.author} deleted their number! The current number is **${numb}**.`)
 		}
 	}
 });
+
+//fail function
+async function fail(reason_for_fail, user_counter, message, user_count) {
+	let formal_fail_reason = ""
+
+	switch (reason_for_fail) {
+		case "2_IN_A_ROW": formal_fail_reason = "You cannot count twice in a row!"; break;
+		case "NUMBER_WRONG": formal_fail_reason = "Wrong number!"; break;
+		default: formal_fail_reason = `UNDEFINED FAIL REASON \`${reason_for_fail}\`!`; break;
+	}
+
+	//disable counting for a set delay
+	canAllCount = false;
+	setTimeout(() => {
+		canAllCount = true;
+	}, ruinDelay);
+
+	if (numb == 0) { //don't add or remove stats if the number was already 0, but still notify them
+
+		if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
+		message.reply(`${message.author} tried to ruin the count, but the number was already **0**!\nNo stats (saves, incorrect numbers) have been modified.\n**${formal_fail_reason}**`)
+		//this is the way to bypass the increment of wrong numbers. It removes and then adds one, cancelling it out.
+		user_counter.decrement('wrongNumbers');
+
+	} else if (user_counter.saves >= 10) { //if the user has saves
+
+		if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
+		user_counter.decrement('saves', {by: 10})
+		message.reply(`${message.author} almost ruined the count, but one of their saves were used!\n${message.author.tag} now has **${(user_counter.saves-10)/10}** saves remaining.\nThe next number is **${numb + 1}** | **${formal_fail_reason}**\n**STREAK LOST AT ${streak}!**`)
+		if (logSaveUses) console.log(`${message.author.tag} used one of their saves, now they have ${(user_counter.saves-10)/10}`)
+
+	} else if (serverSaves >= 1) { //if there are server saves
+
+		if (useCustomEmoji) {message.react(customEmojiList.warn)} else {message.react('⚠️')}
+		serverSaves--
+		message.reply(`${message.author} almost ruined the count, but a server save was used!\n**${serverSaves}** server saves remain.\nThe next number is **${numb+1}** | **${formal_fail_reason}**!\n**STREAK LOST AT ${streak}!**`)
+		if (logSaveUses) console.log(`${message.author.tag} used a server save, now the server has ${serverSaves}}!`)
+
+	} else { //No saves (user and server) and the number was not 0 to start with
+
+		if (useCustomEmoji) {message.react(customEmojiList.ruin)} else {message.react('❌')}
+		message.reply(`${message.author} ruined the count!\nThe next number was **${numb+1}**, but they said **${user_count}**! | **${formal_fail_reason}**!\n**STREAK LOST AT ${streak}!**`)
+		numb = 0
+		lastCounterId = "0"
+		guildDB.update({ lastCounterID: "0" })
+		if (logRuins) console.log(`${message.author.tag} ruined the count at ${numb}!`)
+
+	}
+
+	//modify user stats and streak
+	user_counter.increment('wrongNumbers');
+	streak = 0
+
+	guildDB.update({ count: numb, guildSaves: serverSaves, highscore: highscore, streak: streak })
+}
 
 //check every second asynchronously
 setInterval(async () => {
@@ -348,11 +326,11 @@ setInterval(async () => {
 }, 1000);
 
 //fallbacks just incase something really bad happens
-process.on('uncaughtException', (error, origin) => {
+process.on('uncaughtException', async (error, origin) => {
 	console.log(`❌ Uncaught exception\n-----\n${error.stack}\n-----\nException origin\n${origin}`)
 })
 
-process.on('unhandledRejection', (reason, promise) => {
+process.on('unhandledRejection',  async (reason, promise) => {
 	console.log(`❌ Unhandled Rejection\n-----\n${promise}\n-----\nReason\n${reason}`)
 })
 
